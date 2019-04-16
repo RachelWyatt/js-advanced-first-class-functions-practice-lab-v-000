1788538214474993c99feee7d3eb26e16def3218
Rachel Wyatt <rw126@evansville.edu>
2019-04-16 18:17:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 // Code your solution in this file!
 function logDriverNames(driver) {
-  
+  const stringify = function (agg, el, i, arr) {
+  let stringifiedElement = el.name + ' is $' + el.price + '. ';
+ 
+  if (i === arr.length - 1) {
+    stringifiedElement += 'The total price is $' + (agg.totalPrice + el.price) + '.';
+ 
+    return agg.string + stringifiedElement;
+  }
+ 
+  return {
+    string: agg.string + stringifiedElement,
+    totalPrice: agg.totalPrice + el.price
+  };
+};
+ 
+products.reduce(stringify, { string: '', totalPrice: 0 });
 }
