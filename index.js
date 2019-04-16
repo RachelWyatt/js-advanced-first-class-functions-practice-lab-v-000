360869370c9395dabaa3b42ac0f1b9256f1bc197
Rachel Wyatt <rw126@evansville.edu>
2019-04-16 18:18:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,22 +1,8 @@
 // Code your solution in this file!
-const reduceDriverNames = function (agg, el, i, arr) {
-  let stringifiedElement = el.name + ' is $' + el.price + '. ';
- 
-  if (i === arr.length - 1) {
-    stringifiedElement += 'The total price is $' + (agg.totalPrice + el.price) + '.';
- 
-    return agg.string + stringifiedElement;
-  }
- 
-  return {
-    string: agg.string + stringifiedElement,
-    totalPrice: agg.totalPrice + el.price
-  };
-};
- 
-products.reduce(stringify, { string: '', totalPrice: 0 });
+
 
 
 function logDriverNames(driver) {
+  driver.for_each
   
 }
